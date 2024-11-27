58476111462b7bf8342a51004cbeccec21f68e05
Abbas firdous <abbasfathima40@gmail.com>
2024-11-27 18:05:55 +0530

    hosting0.3

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ const startServer = async () => {
     await connectDb();
     app.listen(PORT, () => {
       console.log(
-        `Server started \nUser: http://localhost:${PORT} \nAdmin: http://localhost:${PORT}/admin/login`
+        `Server started \nUser: https://urbangents.site \nAdmin: https://urbangents.store/admin/login`
       );
     });
   } catch (error) {
--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
 
 const connectDb = async () => {
   try {
+    console.log(MONGO_URI);
     await mongoose.connect(MONGO_URI);
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.log('Error connecting to db', error.message);
+    console.log("Error connecting to db", error.message);
   }
 };
 
