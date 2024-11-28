052370f75ef6d90901c7b53be395615bed399fe6
Abbas firdous <abbasfathima40@gmail.com>
2024-11-28 18:27:04 +0530

    admin customerc code updates

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,27 +1,33 @@
 import Users from "../models/userModel.js";
 
 export const userAuth = async (req, res, next) => {
-  if (req.session.user) {
-    const userData = req.session.user?.email || req.session.user;
-    const user = await Users.findOne({
-      $or: [{ email: userData }, { googleId: userData }],
-    });
-    console.log(user);
-    if (!user) {
-      console.log('user not found')
-      req.session.user = null;
-      return res.status(404).redirect("/user/login");
+  try {
+    if (req.session.user) {
+      const userData = req.session.user?.email || req.session.user;
+      const user = await Users.findOne({
+        $or: [{ email: userData }, { googleId: userData }],
+      });
+
+      if (!user) {
+        req.session.user = null; 
+        return res.redirect("/user/login");
+      }
+      // Handle blocked or inactive user
+      if (user.status || user.status === "undefined") {
+        req.session.user = null; 
+        return res.redirect("/user/login");
+      }
+
+      next(); 
+    } else {
+      res.redirect("/user/login"); // User not logged in
     }
-    console.log('user 2', user)
-    if (user.status || user.status === "undefined") {
-      req.session.user = null;
-      return res.status(403).redirect("/user/login");
-    }
-    next();
-  } else {
-    res.status(401).redirect("/user/login");
+  } catch (error) {
+    console.error("Error in userAuth middleware:", error);
+    res.status(500).send("Internal Server Error");
   }
 };
+
 
 export const isUser = async (req, res, next) => {
   if (req.session.user) {
